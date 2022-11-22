7cd5556636ec7ba1db5e5ec5f976002c4e1ee067
smthe <smthe@10.72.17.69>
2022-11-22 01:11:26 -0500

    can now traverse between all of the screens

---
+++ photos09/src/photos/CopyMovePhotoController.java
@@ -0,0 +1,48 @@
+package photos;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class CopyMovePhotoController {
+	
+	@FXML
+	private TextField usernameField;
+	private Scene scene;
+	private Stage stage;
+	private Parent root;
+	
+	public void goBack(ActionEvent e) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("currentAlbumDisplay.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+
+}
+public void copy(ActionEvent e) throws IOException {
+	Parent root = FXMLLoader.load(getClass().getResource("currentAlbumDisplay.fxml"));
+	stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+	scene = new Scene(root);
+	stage.setScene(scene);
+	stage.show();
+
+}
+public void move(ActionEvent e) throws IOException {
+	Parent root = FXMLLoader.load(getClass().getResource("currentAlbumDisplay.fxml"));
+	stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+	scene = new Scene(root);
+	stage.setScene(scene);
+	stage.show();
+
+}
+
+	}
+
--- photos09/src/photos/CurrentAlbumToolsController.java
+++ photos09/src/photos/CurrentAlbumToolsController.java
@@ -11,23 +11,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class adminToolsController {
+public class CurrentAlbumToolsController {
 	
 	@FXML
 	private TextField usernameField;
 	private Scene scene;
 	private Stage stage;
 	private Parent root;
-	public void login(ActionEvent e) throws IOException {
-		if(usernameField.getText().compareTo("admin")==0) {
-			Parent root = FXMLLoader.load(getClass().getResource("adminTools.fxml"));
+	
+	public void goBack(ActionEvent e) throws IOException {
+			Parent root = FXMLLoader.load(getClass().getResource("nonAdminHomepage.fxml"));
 			stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
-		}else {
-			
-		}
+
 	}
-	}
+	public void openAlbum(ActionEvent e) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("currentAlbumDisplay.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+
+}
+
+}
 
