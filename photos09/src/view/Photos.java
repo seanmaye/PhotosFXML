d75f9130d36ac33b444e9680ce7b9d0d34edf654
DeYiC <DeYiC@172.31.199.225>
2022-11-29 16:21:37 -0500

    added uapp to Photos

--- photos09/src/view/Photos.java
+++ photos09/src/view/Photos.java
@@ -9,11 +9,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import photos.Album;
 import photos.User;
+import photos.UserApp;
 
 public class Photos extends Application {
+	UserApp uapp = new UserApp();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		uapp = UserApp.readApp();
 		Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
